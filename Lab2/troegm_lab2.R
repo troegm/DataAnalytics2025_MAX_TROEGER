d8408e6d49c4a2ab1d12fbba8861aae096a99e66
Max Troeger <maxtroeger@folsom-wl-44.dynamic2.rpi.edu>
2025-02-07 14:59:16 -0500

    Lab 2 work added

--- Lab2/troegm_lab2.R
+++ Lab2/troegm_lab2.R
@@ -1,128 +1,88 @@
 # Lab 2
 # Max Troeger (troegm@rpi.edu)
+# January 31, 2025
 
-# Decision Trees --
-# Classification Tree
-# Install the following libararies/packages in RStudio library(rpart)
-library(rpart.plot)
-# we will be using the iris dataset
-iris
-dim(iris)
-# creating a sample from the iris dataset
-s_iris <- sample(150,100)
-s_iris
-# creat testing and training sets
-iris_train <-iris[s_iris,]
-iris_test <-iris[-s_iris,]
-dim(iris_test)
-dim(iris_train)
+library(sandwich)
+library(lmtest)
+library(readr)
+library(ggplot2)
+NY_House_Dataset <- read_csv("Documents/School/data_analytics/repo/Lab2/NY-House-Dataset.csv")
 
-# generate the decision tree model
-dectionTreeModel <- rpart(Species~., iris_train,
-                          method = "class")
-dectionTreeModel
-#plotting the decision tree model using
-#rpart.plot() function
-rpart.plot(dectionTreeModel)
+# Remove outliers and erroneous values
+NY_House_Dataset <- na.omit(NY_House_Dataset)
+NY_House_Dataset <- subset(NY_House_Dataset,PRICE<195000000)
+NY_House_Dataset <- subset(NY_House_Dataset,PROPERTYSQFT!=2184.207862)
 
-# abalone dataset from UCI repository
-# reading the dataset from UCI repository URL
-abalone <- read.csv(url("https://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data"), header = FALSE, sep = ",")
-# Column names
-colnames(abalone) <- c("sex", "length", 'diameter', 'height', 'whole_weight', 'shucked_wieght', 'viscera_wieght', 'shell_weight', 'rings' )
-# summary on abalone
-summary(abalone)
-# structure of the abalone data
-str(abalone)
-# summary of the abalone rings column
-summary(abalone$rings)
+NY_House_Dataset$LOGPRICE <- log10(NY_House_Dataset$PRICE)
+NY_House_Dataset$LOGPROPERTYSQFT <- log10(NY_House_Dataset$PROPERTYSQFT)
 
-# As shown above, the “rings” variable has a range between 1-29.
-# This is the variable that we want to predict, and predicting this many levels
-# might not give us the insight we’re looking for.
-# For now, we’ll break the rings variable
-# into 3 levels" “young” for abalones less than 8, “adult” for abalones between 8-11,
-# and “old” for abalones older than 11.
-abalone$rings <- as.numeric(abalone$rings)
-abalone$rings <- cut(abalone$rings, br=c(-1,8,11,35), labels = c("young", 'adult', 'old'))
-abalone$rings <- as.factor(abalone$rings)
-summary(abalone$rings)
-# remove the "sex" variable in abalone, because KNN requires all numeric variables for prediction #
-z <- abalone
-aba <- abalone
-aba$sex <- NULL
+plot(NY_House_Dataset$LOGPROPERTYSQFT,NY_House_Dataset$LOGPRICE)
 
-# normalize the data using min max normalization
-normalize <- function(x) {return ((x - min(x)) / (max(x) - min(x))) }
-aba[1:7] <- as.data.frame(lapply(aba[1:7], normalize))
-summary(aba$shucked_wieght)
-# After Normalization, each variable has a min of 0 and a max of 1.
-# in other words, values are in the range from 0 to 1.
-# We’ll now split the data into training and testing sets.
-ind <- sample(2, nrow(aba), replace=TRUE, prob=c(0.7, 0.3))
-KNNtrain <- aba[ind==1,]
-KNNtest <- aba[ind==2,]
-sqrt(2918)
+##
+# Model 1 - LOGPRICE vs LOGPROPERTYSQFT
+##
 
-# make k equal to the square root of 2918, the number of observations in the training set.
-# sqrt(2918) ~= 54.01852 round it to 55 and use k = 55
-# We usually take an Odd number for k value,
-# knn model
-# knn() is in the "class" library. Make sure to install it first on your RStudio.
-library(class)
-help("knn") # Read the knn documentation on RStudio.
-KNNpred <- knn(train = KNNtrain[1:7], test = KNNtest[1:7], cl = KNNtrain$rings, k = 55)
-KNNpred
-table(KNNpred)
+fit1 <- lm(LOGPRICE~LOGPROPERTYSQFT,data=NY_House_Dataset)
+#coeftest(fit1)
+summary(fit1)
 
-# iris dataset is from UCI ML repository.
-library(ggplot2) # we will use ggplot2 to visualize the data.
-head(iris) # first 6 rows of the
-str(iris) # take a look at the structure of the iris data using str() function in R.
-# dataset has 150 observations equally distributed observations among
-# the three species: Setosa, Versicolor and Verginica.
-summary(iris) # summary statistics of all the 4 variables Sepal.Length,Sepal.Width,
-# Petal.Length and Petal.Width
-help("sapply")
-sapply(iris[,-5], var)
-summary(iris)
+## Plotting LOGPRICE vs LOGPRICE
+ggplot(NY_House_Dataset, aes(x = LOGPROPERTYSQFT, y = LOGPRICE)) +
+  geom_point() +
+  stat_smooth(method = "lm", col="red")
 
-# plot Sepal.Length Vs Sepal.Width using ggplot
-ggplot(iris,aes(x = Sepal.Length, y = Sepal.Width, col= Species)) + geom_point()
-# plot Petal.Length Vs Sepal.Width using ggplot
-ggplot(iris,aes(x = Petal.Length, y = Petal.Width, col= Species)) + geom_point()
-# kmeans clustering
-# Read the documentation for kmeans() function
-# https://stat.ethz.ch/R-manual/R-devel/library/stats/html/kmeans.html
-set.seed(300)
-k.max <- 12
-# tot.withinss = Total within-cluster sum of square
-# iter.max = the maximum number of iterations allowed
-# nstart = if centers is a number, how many random sets should be chosen.
-wss<- sapply(1:k.max,function(k){kmeans(iris[,3:4],k,nstart = 20,iter.max = 20)$tot.withinss})
+ggplot(fit1, aes(x = .fitted, y = .resid)) + geom_point()
 
-wss # within sum of squares.
-plot(1:k.max,wss, type= "b", xlab = "Number of clusters(k)", ylab = "Within cluster sum of squares") 
-icluster <- kmeans(iris[,3:4],3,nstart = 20)
-table(icluster$cluster,iris$Species)
-# In the table we can see that most of the observations have been clustered correctly
-# however, 2 of the versicolor have been put in the cluster with all the virginica
-# and 4 of the verginica have been put in cluster 3 which mostly has versicolor.
+##
+# Model 2 - LOGPRICE vs LOGPROPERTYSQFT & BEDS
+##
 
-# Classification ctrees
-# iris data set
-# Install the following libararies/packages library(rpart)
-library(rpart.plot)
-# we will be using the iris dataset
-iris
-dim(iris) # check the dimensions of the iris dataset
-# creating a sample from the iris dataset s_iris <- sample(150,100)
-s_iris
-# creat testing and training sets iris_train <-iris[s_iris,]
-iris_test <-iris[-s_iris,] 
-dim(iris_test)
-dim(iris_train)
-# generate the decision tree model
-dectionTreeModel <- rpart(Species~., iris_train, method = "class") 
-dectionTreeModel
-#plotting the decision tree model using rpart.plot() function rpart.plot(dectionTreeModel)
+fit2 <- lm(LOGPRICE~LOGPROPERTYSQFT + BEDS,data=NY_House_Dataset)
+#coeftest(fit2)
+summary(fit2)
+
+## Plotting LOGPRICE vs BEDS
+ggplot(NY_House_Dataset, aes(x = BEDS, y = LOGPRICE)) +
+  geom_point() +
+  stat_smooth(method = "lm", col="red")
+
+ggplot(fit2, aes(x = .fitted, y = .resid)) + geom_point()
+
+##
+# Model 3 - LOGPRICE vs LOGPROPERTYSQFT, BEDS, & BATH
+##
+
+fit3 <- lm(LOGPRICE~LOGPROPERTYSQFT+BATH+BEDS,data=NY_House_Dataset)
+#coeftest(fit3)
+summary(fit3)
+
+## Plotting LOGPRICE vs BATH
+ggplot(NY_House_Dataset, aes(x = BATH, y = LOGPRICE)) +
+  geom_point() +
+  stat_smooth(method = "lm", col="red")
+
+ggplot(fit3, aes(x = .fitted, y = .resid)) + geom_point()
+
+
+##
+# Boxplot & QQ Plots
+##
+boxplot(NY_House_Dataset$LOGPROPERTYSQFT,NY_House_Dataset$BEDS,NY_House_Dataset$BATH)
+
+qqnorm(NY_House_Dataset$LOGPROPERTYSQFT)
+qqline(NY_House_Dataset$LOGPROPERTYSQFT) 
+
+qqnorm(NY_House_Dataset$BATH)
+qqline(NY_House_Dataset$BATH) 
+
+qqbeta(NY_House_Dataset$BEDS)
+qqline(NY_House_Dataset$BEDS) 
+
+ggplot(NY_House_Dataset, aes(x=LOGPROPERTYSQFT)) +
+  stat_ecdf() +
+  
+ggplot(NY_House_Dataset, aes(x=BATH)) +
+  stat_ecdf()
+
+ggplot(NY_House_Dataset, aes(x=BEDS)) +
+  stat_ecdf()
